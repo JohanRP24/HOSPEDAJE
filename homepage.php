a9a95f6d47d48fc6e0e0c6bba4dccafe423ed8d6
JohanRP24 <johan24andres24@gmail.com>
2024-08-10 14:38:04 -0500

    Se agrega pagina de inicio

--- homepage.php
+++ homepage.php
@@ -10,12 +10,41 @@ include("connect.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/stylehome.css">
     <title>Homepage</title>
 </head>
 <body>
-    <div style="text-align:center; padding:15%;">
-      <p  style="font-size:50px; font-weight:bold;">
-       Hello  <?php 
+<header>
+        <nav>
+        <h1>HOTEL ESTRELLA DE MAR</h1>
+        <a href="logout.php">SALIR</a>
+    </nav>
+    </header>
+
+    <div>
+        <!--Boton de clientes-->
+        <button class="custom-button"> <a href="clientes.html">
+            <img src="./img/clientes.png" alt="Clic para ver clientes">
+        </button> </a>
+    </div>
+     <!--Boton de habitaciones-->
+     <div>
+     <button class="custom-button"> <a href="habitacion.html">
+            <img src="./img/habitacion.png" alt="Clic para ver habitaciones"></button>
+            </a>
+            </div>
+    <div>
+        <button class="custom-button"> <a href="reservas.html">
+            <img src="./img/reservas.png" alt="Clic para ver reservas"> 
+        </button> </a>
+        </div>           
+    <div>
+        <button class="custom-button"><a href="facturas.html">
+            <img src="./img/factura.png" alt="Clic para ver factura">
+        </button> </a>
+    </div>
+
+    <div
        if(isset($_SESSION['email'])){
         $email=$_SESSION['email'];
         $query=mysqli_query($conn, "SELECT users.* FROM `users` WHERE users.email='$email'");
@@ -24,9 +53,8 @@ include("connect.php");
         }
        }
        ?>
-       :)
-      </p>
-      <a href="logout.php">Logout</a>
+     
+      
     </div>
 </body>
 </html>
